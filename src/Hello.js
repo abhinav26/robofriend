c2d515a4842f62f7ff1984afcb79af163af3fe46
abhinav kulkarni <abhinav2697@gmail.com>
2020-02-29 23:34:45 +0530

    first commit

---
+++ src/Hello.js
@@ -0,0 +1,13 @@
+import React,{Component} from 'react';
+class Hello extends Component{
+    render(){
+        return(
+        <div>
+            <h1>Hello World</h1>
+            <p>Welcome to react</p>
+        </div>
+        );
+    }
+}
+
+export default Hello;
