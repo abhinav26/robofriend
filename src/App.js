76c63ae7679c0b40ab56ac6259f3c67566140200
abhinav kulkarni <abhinav2697@gmail.com>
2020-03-01 17:40:21 +0530

    commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import CardList from './CardList';
+
+import{robots} from './robots';
+const App=()=>{
+    return(
+        <h1>Robo Friends</h1>
+        <CardList robots={robots}/>
+    );
+}
+export default App;
--- src/index.js
+++ src/index.js
@@ -2,9 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import 'tachyons';
+//import Card from './Card';
+import SearchBox from './SearchBox';
+import App from './App';
+import CardList from './CardList';
+import {robots} from './robots';
 //import registerServiceWorker from './registerServiceWorker';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<H />, document.getElementById('root'));
+ReactDOM.render(<App />,document.getElementById('root'));
 //registerServiceWorker();
 serviceWorker.unregister();
